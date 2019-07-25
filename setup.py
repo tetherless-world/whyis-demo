026e041d682c7a1e612ed81b8747a206bc6f66a6
Minor Gordon <gordom6@rpi.edu>
2019-07-25 14:53:08 -0400

    check in MCBK demo as-is

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='MCBK Demo',
+      version='0.1',
+      description='An example knowledge graph for MCBK.',
+      author='Jim McCusker',
+      packages=[
+          
+          ],
+     )
